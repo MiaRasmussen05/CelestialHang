1e60388236f4732cbf287c193f71518e975e8ff3
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-12-17 09:42:36 +0000

    Create function for rules

--- run.py
+++ run.py
@@ -55,6 +55,10 @@ def welcome_player():
     print("\n" + f"""                          Welcome {name}
                I hope you have fun and good luck!""" + "\n")
     time.sleep(1)
+    separator()
+    rules()
+    separator()
+    time.sleep(1)
 
 
 def separator():
@@ -64,3 +68,30 @@ def separator():
     print("""
 -.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
         """)
+
+
+def rules():
+    """
+    Inform the player about the game rules
+    Implement color for the rules function
+    """
+    print(Fore.CYAN + """                     Here comes the rules...""" "\n")
+    time.sleep(1)
+    print("1. Try to see if you can guess the celestial themed word" + "\n")
+    time.sleep(0.6)
+    print("""2. You play by inputing one letter, if the letter is in the word
+   then you get another change but is the letter not then you lose a life""")
+    time.sleep(0.6)
+    print("""
+3. Can you guess the word before the person gets hung or it's game over.""")
+    time.sleep(0.6)
+    print("""
+4. Remeber choose the difficulty level carefully the higher you go the harder
+   the word is to guess. However the more points you get as well.
+   For each won game - Easy gives 1 point, Medium gives 2 points,
+   Hard gives 3 points, and the special level gives 4 points""")
+    time.sleep(0.6)
+    print("""
+5. Hidden in the scores there is a key. Are you the one that can find it
+   to unlock the special level. Only one score will allow you to see the key,
+   but you will still unlock the hidden level by passing it!""" + Fore.WHITE)
