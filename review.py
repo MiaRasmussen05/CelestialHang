3f0b0b9d78cd4b3a42fad2daf586ccb08e5ee6bf
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-12-17 11:05:50 +0000

    Add stars function with input

--- review.py
+++ review.py
@@ -15,3 +15,16 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('celestial_hang')
+
+
+def stars():
+    """
+    Let the visitor give stars and review of the game
+    while putting them on a spreadsheet
+    """
+    print("""
+-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+        """)
+    print("Please take a second to give us a review between 0 and 3 stars!")
+    print("Stars e.g: 2.7")
+    classification = input("Give stars here:\n").strip(' ').replace(",", ".")
