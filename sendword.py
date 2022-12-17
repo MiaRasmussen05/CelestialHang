a362c3e031b15ce360c552e391a7bae7fcba8959
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-12-17 10:58:07 +0000

    Add while loop to catch errors for invalid input data

--- sendword.py
+++ sendword.py
@@ -3,6 +3,7 @@ Files to store the functions, scope and credits for the player
 to be able to input their own word for review
 """
 import time
+import re
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -34,3 +35,45 @@ def new_words():
    Maybe a word you would like to see become apart of the game
                       Then now is your change!""")
     time.sleep(0.7)
+
+    while True:
+        question = input("""
+          So do you have one or more? yes = y, no = n:\n""").lower().strip(' ')
+        if question != "n":
+            print("""
+-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+            """)
+        time.sleep(0.3)
+        while True:
+            if question == "y":
+                print("""
+If more then one word, they should be separated by commas.
+Up till 5 words is permitted.
+E.g: Virgo,Libra,Aries,Leo,Cancer""")
+                ideas = input("\nEnter your word/s here:\n").strip(' ')
+                ideas_value = ideas.split(",")
+                if not re.match(r'^[\w\s]+(,\s*[\w\s]+)*$', ideas):
+                    print("Please enter a valid string of words.\n")
+                    print("""
+-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+            """)
+                else:
+                    if 1 <= len(ideas_value) <= 5:
+                        return ideas_value
+                    else:
+                        print("""
+        Please enter between 1 and 5 words, separated by commas\n""")
+                        print("""
+-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+            """)
+            elif question == "n":
+                print("""
+-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+            """)
+                return None
+            else:
+                print("                   I am sorry I didn't get that...")
+                print("""
+-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-
+            """)
+                break
